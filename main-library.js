b86d0c0cfe5cbd19426d1b85942e49460b2c7ca7
charle.rae <itscharlerae@gmail.com>
2020-12-15 22:53:39 -0400

    adds User class to main-library file

--- main-library.js
+++ main-library.js
@@ -10,5 +10,19 @@
 
 // user class
 class User {
-    
+    constructor(name) {
+        this.name = name;
+    }
+
+    // accessor method
+    getName() {
+        return this.name;
+    }
+
+    // mutator method
+    changeName(newName) {
+        this.name = newName;
+    }
 }
+
+const charle = new User('charle');
